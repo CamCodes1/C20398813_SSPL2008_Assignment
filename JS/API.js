31db70ca7998f7e7f10b2c9b5b7d65057c88a1bb
Eoinot <134057872+CamCodes1@users.noreply.github.com>
2023-05-22 20:29:18 +0100

    final code

---
+++ JS/API.js
@@ -0,0 +1,70 @@
+let May22 = 'Weather:' + " " +
+		'Temperature: 11.4°C,' + " " +
+		'Rain: 0mm,' + "-" +
+		'Visability: 24,140m,' + " " +
+		'Wind speed: 21.5 KM/H';
+let May23 = 'Weather:' + " " +
+		'Temperature: 10.7°C,' + " " +
+		'Rain: 0mm,' + " " +
+		'Visability: 24,140m,' + " " +
+		'Wind speed: 7.3 KM/H';
+let May24 = 'Weather:' + " " +
+		'Temperature: 12.4°C,' + " " +
+		'Rain: 0mm,' + " " +
+		'Visability: 24,140m,' + " " +
+		'Wind speed: 11 KM/H';
+let May25 = 'Weather:' + " " +
+		'Temperature: 14.4°C,' + " " +
+		'Rain: 0mm,' + " " +
+		'Visability: 24,140m,' + " " +
+		'Wind speed: 14.8 KM/H';
+let May26 = 'Weather:' + " " +
+		'Temperature: 15.2°C,' + " " +
+		'Rain: 0mm,' + " " +
+		'Visability: 24,140m,' + " " +
+		'Wind speed: 13 KM/H';
+let May27 = 'Weather:' + " " +
+		'Temperature: 15.9°C,' + " " +
+		'Rain: 0mm,' + " " +
+		'Visability: 24,140m,' + " " +
+		'Wind speed: 1 KM/H';
+let May28 = 'Weather:' + " " +
+		'Temperature: 12.9°C,' + " " +
+		'Rain: 0mm,' + " " +
+		'Visability: 24,140m,' + " " +
+		'Wind speed: 12.6 KM/H';
+
+function getMay22() {
+	document.getElementById("Result-22").innerHTML = document.write(May22).value;
+	//alert("you clicked me");
+}
+
+function getMay23() {
+	document.getElementById("Result-23").innerHTML = document.write(May23).value;
+	//alert("you clicked me");
+}
+
+function getMay24() {
+	document.getElementById("Result-24").innerHTML = document.write(May24).value;
+	//alert("you clicked me");
+}
+
+function getMay25() {
+	document.getElementById("Result-25").innerHTML = document.write(May25).value;
+	//alert("you clicked me");
+}
+
+function getMay26() {
+	document.getElementById("Result-26").innerHTML = document.write(May26).value;
+	//alert("you clicked me");
+}
+
+function getMay27() {
+	document.getElementById("Result-27").innerHTML = document.write(May27).value;
+	//alert("you clicked me");
+}
+
+function getMay28() {
+	document.getElementById("Result-28").innerHTML = document.write(May28).value;
+	//alert("you clicked me");
+}
---
+++ JS/Java.js
@@ -0,0 +1,4 @@
+function myFunction() {
+	document.getElementById("result").innerHTML = document.getElementById('date').value + " " +document.getElementById('time').value + " " + document.getElementById('parties').value + " " + document.getElementById('incident').value;
+	//alert("you clicked me");
+}
